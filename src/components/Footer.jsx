9ccf93a913ceae61f1f8a453de52d9348440fce1
Aravindselvan <150777603+AravindS2006@users.noreply.github.com>
2025-03-26 22:08:33 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,7 @@ import { FaInstagram, FaXTwitter, FaLinkedin } from "react-icons/fa6";
 const Footer = () => {
   return (
     <footer className="w-full py-4 bg-black-100/55 text-white flex justify-center gap-6">
+      <div><p>Designed By Aravindselvan</p></div>
       <a
         href="https://www.instagram.com/nova__trades/"
         target="_blank"
